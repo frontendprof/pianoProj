ca97620282c5c4bfbe20eec8b8b02fca10b4b695
AbdulMalik <abdumaliksharipov@gmail.com>
2020-05-20 05:38:07 -0500

    Toggle on record button

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
 const BLACK_KEYS = ["s", "d", "g", "h", "j"];
 const WHITE_KEYS = ["z", "x", "c", "v", "b", "n", "m"];
 
+
+const recordBtn = document.querySelector(".record-button");
 const keys = document.querySelectorAll(".key");
 const white_keys = document.querySelectorAll(".key.white");
 const black_keys = document.querySelectorAll(".key.black");
@@ -8,6 +10,8 @@ const black_keys = document.querySelectorAll(".key.black");
 keys.forEach((key) => {
   key.addEventListener("click", () => playNote(key));
 });
+
+recordBtn.addEventListener("click", toggleRecording);
 
 document.addEventListener("keydown", (e) => {
   if (e.repeat) return;
@@ -20,6 +24,25 @@ document.addEventListener("keydown", (e) => {
   if (whiteKeyIndex > -1) playNote(white_keys[whiteKeyIndex]);
   if (blackKeyIndex > -1) playNote(black_keys[blackKeyIndex]);
 });
+
+
+function toggleRecording() {
+  recordBtn.classList.toggle("active");
+
+  if (isRecording()) {
+    startRecording()
+  } else {
+    stopRecording()
+  }
+}
+
+
+function isRecording() {
+  return recordBtn != null && recordBtn.classList.contains("active");
+}
+
+
+
 
 function playNote(key) {
   const noteAudio = document.getElementById(key.dataset.note);
